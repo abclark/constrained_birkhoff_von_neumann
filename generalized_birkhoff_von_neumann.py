7ac6d186a3f30807ab5bce0de08197db6d6c770b
abclark <31861009+abclark@users.noreply.github.com>
2017-10-26 19:44:20 +0100

    Update generalized_birkhoff_von_neumann.py

--- generalized_birkhoff_von_neumann.py
+++ generalized_birkhoff_von_neumann.py
@@ -42,8 +42,6 @@ from pprint import pprint
 
 #global things
 tolerance = np.finfo(np.float).eps*10e10
-X = np.array([[.5, .2,.3], [.5,.5, 0], [.8, 0, .2], [.2, .3, .5]])
-constraint_structure = {frozenset({(0, 0), (0, 1), (0,2)}): (1,1), frozenset({(1, 0), (1, 1), (1,2)}):(1,1), frozenset({(2, 0), (2, 1), (2,2)}):(1,1), frozenset({(3, 0), (3, 1), (3,2)}):(1,1), frozenset({(0, 0), (1, 0), (2,0), (3,0)}):(1,2),  frozenset({(0, 1), (1, 1), (2,1), (3,1)}):(1,1), frozenset({(0, 2), (1, 2), (2,2), (3,2)}):(1,1), frozenset({(0, 0), (1, 0)}):(1,1)}
 S = {index for index, x in np.ndenumerate(X)}
 
 #feasibity_test tests whether all entries of the starting matrix X are in [0,1]. This is essential since each basis matrix has entries

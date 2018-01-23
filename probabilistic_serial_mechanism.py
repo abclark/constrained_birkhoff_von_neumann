8b5711cc90c1472162af1589c8cf3c778ec895f9
abclark <31861009+abclark@users.noreply.github.com>
2018-01-23 19:25:06 +0000

    Update probabilistic_serial_mechanism.py

--- probabilistic_serial_mechanism.py
+++ probabilistic_serial_mechanism.py
@@ -19,6 +19,7 @@ Copyright 2017 Aubrey Clark.
 probabilistic_serial_mechanism.py is free software: you can redistribute it and/or modify it 
 under the terms of the GNU General Public License as published by the Free Software Foundation, 
 either version 3 of the License, or (at your option) any later version.
+
 """
 
 import numpy as np

fc36fc43063cb8099f13f9bde2ec7813e716d794
abclark <31861009+abclark@users.noreply.github.com>
2018-02-08 11:57:53 +0000

    Update constrained_birkhoff_von_neumann.py

--- constrained_birkhoff_von_neumann.py
+++ constrained_birkhoff_von_neumann.py
@@ -46,10 +46,10 @@ tolerance = np.finfo(np.float).eps*10e10
 def feasibility_test(X, constraint_structure):
   S = {index for index, x in np.ndenumerate(X)}
   if any(X[i]<0 or X[i]>1 for i in S):
-    sys.exit("matrix entries must be between zero and one")
+    print("matrix entries must be between zero and one")
   for key, value in constraint_structure.items():
     if sum([X[i] for i in key]) < value[0] or sum([X[i] for i in key]) > value[1]:
-      sys.exit("matrix entries must respect constraint structure capacities")
+      print("matrix entries must respect constraint structure capacities")
  
 #bihierarchy_test attempts to decompose the constraint structure into a bihierarchy.
 #The user is informed if this is not possible.
@@ -72,7 +72,7 @@ def bihierarchy_test(constraint_structure):
       target.append(idx)
     if len(listofA) + len(listofB) == len(constraint_sets):
       return [[constraint_set_ordering[i] for i in listofA], [constraint_set_ordering[i] for i in listofB]]
-  sys.exit("this constraint structure is not a bihierarchy")
+  print("this constraint structure is not a bihierarchy")
 
 #graph_constructor takes a target matrix X and a bihierarchy = [A,B],
 #constructs a directed weighted graph G
